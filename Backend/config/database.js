064fa8b331921bc7be1220ad518b9a22da655128
MuhammedYahiya <yahiyahmhd@gmail.com>
2024-02-12 22:08:27 +0530

    fix:mongodb connection error fixed

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -3,8 +3,9 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      "mongodb://localhost:27017/blackcoffer"
+      "mongodb+srv://yahiyahmhd:Messi10%40@cluster0.wnkdobs.mongodb.net/blackcoffer"
     );
+
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
